bfd47e0488818969f4ff6a7e60866abdb782ade6
Your Name <ququ@qucmpution.onmicrosoft.com>
2021-01-10 15:06:43 +0800

    1

---
+++ 注释.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+//1、单行注释
+
+//2、多行注释
+
+
+/*
+这是多行注释
+
+
+*/
+
+
+
+
+int main()
+{
+    //11行代码含义是在屏幕中输出Hello World
+    cout << "Hello World" << endl;
+
+    system("pause");
+
+    return 0;
+}
