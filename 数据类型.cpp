02a8231ba0b7ef1d893aaec17bef286d6800ada8
Your Name <ququ@qucmpution.onmicrosoft.com>
2021-01-21 15:34:45 +0800

    new write

--- 数据类型.cpp
+++ 数据类型.cpp
@@ -8,11 +8,18 @@ int main() {
 	short numl = 10;
 
 	//2、整型
+	int num2 = 10;
 
 	//3、长整型
+	long num3 = 10;
 
 	//4、长长整型
+	long long num4 = 10;
 
+	cout << "num1 = " << numl << endl;
+	cout << "num2 = " << num2 << endl; 
+	cout << "num3 = " << num3 << endl;
+	cout << "num4 = " << num4 << endl;
 	system("pause");
 
 	return 0;
