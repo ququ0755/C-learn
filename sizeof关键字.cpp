044c8b443d8697b4ca0916a83bb1cd0e0197c1f2
Your Name <ququ@qucmpution.onmicrosoft.com>
2021-01-22 19:56:39 +0800

    1

--- sizeof关键字.cpp
+++ sizeof关键字.cpp
@@ -3,10 +3,15 @@ using namespace std;
 
 int main() {
 
+	
+	
 	//整型：short (2) int (4) long (4) long long (8)
 	//可以利用sizeof求出数据类型占用内存的大小
 	//语法：sizeof（数据类型/变量）
+	
+	
 
+	
 
 	short numl = 10;
 	cout << "short占用内存空间为：" << sizeof(short) << endl;
