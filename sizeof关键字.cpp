9959f6bc2bb891accd7c9a5fbab72f590cc3ecf3
Your Name <ququ@qucmpution.onmicrosoft.com>
2021-01-22 15:38:01 +0800

    1

---
+++ sizeof关键字.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	//整型：short (2) int (4) long (4) long long ()
+	//可以利用sizeof求出数据类型占用内存的大小
+	//语法：sizeof（数据类型/变量）
+
+
+	short numl = 10;
+	cout << "short占用内存空间为：" << sizeof(short) << endl;
+
+
+
+
+
+
+
+
+	system("pause");
+
+	return 0;
+}
--- 数据类型.cpp
+++ 数据类型.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int main() {
+int q() {
 
 	//整型
 	//1、短整型 (-32768 ~ 32767)
