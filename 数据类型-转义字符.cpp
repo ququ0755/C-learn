103020ea47e82b4573b2823e19fef3b4bf1dc052
Fan Qu <62249086+ququ0755@users.noreply.github.com>
2021-01-25 14:14:41 +0800

    123
    new wrieting

---
+++ 数据类型-转义字符.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //转义字符
+
+    //换行符 \n
+
+    cout << "hello world\n";
+
+    //反斜杠 \\
+
+    cout << "\\" <<endl;
+    
+    //水平制表符 \t    作用可以整齐输出数据
+
+    cout << "aaaa\thelloworld" <<endl;
+    cout << "aa\thelloworld" <<endl;
+    cout << "aaaaa\thelloworld" <<endl;
+   
+
+
+    system("pause");
+    return 0;
+
+}
