cbbc770fb278aa663bcd2266720ea112cced9cf9
Fan Qu <62249086+ququ0755@users.noreply.github.com>
2021-01-27 14:35:24 +0800

    new learning

---
+++ 数据类型-布尔类型.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    bool flag = true;
+    cout << flag << endl; //1
+
+    flag = false;
+    cout << flag <<endl; //0
+
+    cout << "size of bool = " << sizeof(bool) << endl;
+
+
+    system("pause")
+
+
+    return 0;
+
+}
