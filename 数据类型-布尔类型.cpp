50615448753803d4d44af27d1418d55058d6b8b8
Fan Qu <62249086+ququ0755@users.noreply.github.com>
2021-01-28 14:43:38 +0800

    new learn

--- 数据类型-布尔类型.cpp
+++ 数据类型-布尔类型.cpp
@@ -2,19 +2,13 @@
 using namespace std;
 
 int main(){
-    
-    //1 创建bool数据类型
-    bool flag = true;//true代表真
+
+    bool flag = true;
     cout << flag << endl; //1
 
-    flag = false;//false代表假
+    flag = false;
     cout << flag <<endl; //0
 
-    
-    //本质上1代表真，0代表假
-    
-    //查看bool内存空间
-    
     cout << "size of bool = " << sizeof(bool) << endl;
 
 
