64925d808677b67cd81e75cef205189ace50df86
Fan Qu <62249086+ququ0755@users.noreply.github.com>
2021-01-28 14:02:37 +0800

    Update 数据类型-布尔类型.cpp

--- 数据类型-布尔类型.cpp
+++ 数据类型-布尔类型.cpp
@@ -2,13 +2,19 @@
 using namespace std;
 
 int main(){
-
-    bool flag = true;
+    
+    //1 创建bool数据类型
+    bool flag = true;//true代表真
     cout << flag << endl; //1
 
-    flag = false;
+    flag = false;//false代表假
     cout << flag <<endl; //0
 
+    
+    //本质上1代表真，0代表假
+    
+    //查看bool内存空间
+    
     cout << "size of bool = " << sizeof(bool) << endl;
 
 
